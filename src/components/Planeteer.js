7b6e0bd52a258f0aee556bde9b7def97e549329b
amaclarty <100883044+amaclarty@users.noreply.github.com>
2022-07-28 15:17:29 -0400

    code challenge

--- src/components/Planeteer.js
+++ src/components/Planeteer.js
@@ -1,23 +1,41 @@
 import React from "react";
+import {useState} from 'react'
 
-function Planeteer() {
+
+function Planeteer({planeteer}) {
+
+  const [planeteerLocation, setPlaneteerLocation] =useState([])
+
+  const checkPlaneteerLocation = (planeteer) => {
+    return (planeteer).fromUSA ? setPlaneteerLocation('from USA') : setPlaneteerLocation('Working overseas')
+  }
+
+  
+  // const profileClickHandler = () => {
+
+  // }
+
   return (
     <li className="cards__item">
       <div className="card">
         <img
-          src={"RENDER IMAGE"}
-          alt={"RENDER PERSON NAME"}
+          src={planeteer.pictureUrl}
+          alt={planeteer.name}
           className="card__image"
+          // onClick=''
         />
         <div className="card__content">
-          <div className="card__title">{"RENDER NAME"}</div>
+          <div className="card__title">{planeteer.name}</div>
           <p className="card__text">{"CONDITIONALLY RENDER BIO OR QUOTE"}</p>
           <div className="card__detail">
-            <p>{"RENDER TWITTER HANDLE"}</p>
-            <p>
-              {
-                "CONDITIONALLY RENDER WHETHER THE PERSON IS USA-BASED OR WORKING OVERSEAS"
-              }
+            <p>{planeteer.twitter}</p>
+            <p>{
+             <p>
+              {/* I got stuck on this creating a loop. No idea why. RIP*/}
+              {/* {checkPlaneteerLocation(planeteer)}
+                {planeteerLocation} */}
+              </p>
+            }
             </p>
           </div>
         </div>
--- src/components/PlaneteersContainer.js
+++ src/components/PlaneteersContainer.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Planeteer from "./Planeteer";
-
+import {useState, useEffect} from 'react'
+ 
 function PlaneteersContainer() {
+  const [planeteers, setPlaneteers] =useState([])
+
+  useEffect( () => {
+      const getPlaneteers = async () => {
+      let req = await fetch("http://localhost:8003/planeteers")
+      let res = await req.json()
+      setPlaneteers(res)
+      }
+    getPlaneteers()
+    }, [])
+    console.log(planeteers)
+
   return (
     <ul className="cards">
-      {/* render a list of <Planeteer> components in here */}
+      {planeteers.map((planeteer) => {
+        return(
+        <Planeteer planeteer={planeteer} key={planeteer.id}/>
+        )
+      })}
     </ul>
   );
 }
